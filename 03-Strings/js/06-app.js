685ade6091db56c1608f7aea18b2b3e23fc5e418
PotterFH <ivan_2333@outlook.com>
2022-07-12 12:25:27 -0500

    Seccion String finalizada

--- 03-Strings/js/06-app.js
+++ 03-Strings/js/06-app.js
@@ -7,3 +7,14 @@ console.log(texto);
 
 console.log( `${producto} ${texto} !!!`);
 
+
+//Split para dividir un string
+
+const actividad = 'Estoy aprendiendo JavaScript';
+console.log(actividad.split(" "));
+
+const hobbies = 'Leer, caminar, escuchar musica, escribir, aprender a programar';
+console.log(hobbies.split(','));
+
+const tweet = '#Aprendiendo JavaScript';
+console.log(tweet.split('#'));
---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,15 @@
+const producto = 'Monitor de 24 pulgadas';
+
+//toUpperCase:  texto a mayusculas
+
+console.log(producto.toUpperCase());
+console.log(producto.toLowerCase());
+
+const email = 'CORREO@CORREO.COM';
+console.log(email.toLowerCase());
+
+//toString: Convertir un dato a tipo int
+const precio = 300;
+console.log(precio);
+console.log(precio.toString());
+
