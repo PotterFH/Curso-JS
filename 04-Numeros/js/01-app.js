14ed1acacc14c6b4682bbbb1046ae8052f1815bd
PotterFH <ivan_2333@outlook.com>
2022-07-13 10:14:52 -0500

    Seccion numeros 2

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,15 @@
+//Creacion de numeros 
+
+//Formas de crear numeros
+const numero1 = 20;
+const numero2 = 30;
+const numero3 = 40.20;
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+//Forma no tan comun
+const numero = new Number(80);
+console.log(numero);
---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,28 @@
+//Operaciones con numeros 
+
+const numero1 = 20;
+const numero2 = 30;
+
+let resultado;
+
+// Suma
+resultado = numero1 + numero2;
+console.log(resultado);
+
+// Resta
+resultado = numero1 - numero2;
+console.log(resultado);
+
+// Multiplicacion
+resultado = numero1 * numero2;
+console.log(resultado);
+
+
+//Division
+resultado = numero1 / numero2;
+console.log(resultado);
+
+
+//Modulo
+resultado = numero1 % numero2;
+console.log(resultado);
