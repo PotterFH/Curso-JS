96b399e437c113732043cf9989479291025b4ee1
PotterFH <ivan_2333@outlook.com>
2022-07-20 19:39:55 -0500

    Seccion Booleanos finalizada

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autenticado = true;
+
+if(autenticado){
+console.log('Puedes ver netflix');
+}else{
+    console.log('No no puedes ver Netflix');
+}
+
+//operador ternario
+
+console.log( autenticado ? 'Si esta autenticado' : 'No esta autenticado');
