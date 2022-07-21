e279eae7a56b650160423b3d121daaee88fd4255
PotterFH <ivan_2333@outlook.com>
2022-07-20 19:29:34 -0500

    Seccion operadores terminada

---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,19 @@
+const numero1 = 20;
+const numero2 = '20';
+const numero3 = 30;
+
+// si 2 numeros son iguales
+console.log(numero1 == numero2);
+console.log(numero1 == numero3);
+
+//comparador escricto
+console.log(numero1 === parseInt(numero2));
+console.log(numero1 === numero3);
+
+//Comporbar sin son Diferentes
+const password1 = 'admin';
+const password2 = 'Admin';
+
+console.log(password1 != password2);
+console.log(numero1 != numero2);
+console.log(numero1 !== numero2);
---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,13 @@
+//tipos null y undefined
+
+let numero1;
+
+console.log(numero1);
+console.log(typeof numero1);
+
+
+//null
+let numero2 = null;
+console.log(numero2);
+console.log(typeof numero2);
+
